ef499ca844868273b65ca8a4e09fd8add2b0c826
gleissonhcd <gleissonhcd@gmail.com>
2021-07-29 21:57:14 -0300

    Adicionando o laço de repetição while

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,28 @@
-print("Bem vindo no jogo de Advinhação")
+print("*******************************")
+print("Bem vindo ao jogo de Advinhação")
+print("*******************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou ==> ", chute)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Parabéns você acertou o número!!!")
+        break
+    else:
+        if(maior):
+            print("Você errou seu chute foi maior que o número secreto")
+        elif(menor):
+            print("Você errou o seu número foi menor que o número secreto")
+        rodada += 1
+
+print("Fim de Jogo!!!")
