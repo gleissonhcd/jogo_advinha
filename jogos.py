b627fcef5d7d35772a498c85a37ba80633420331
Gleisson Henrique <40076908+gleissonhcd@users.noreply.github.com>
2021-08-05 22:51:13 -0300

    Add files via upload
    Adicionando Forca.

---
+++ jogos.py
@@ -0,0 +1,20 @@
+import forca
+import adivinhacao
+
+def escolher_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    jogo = int(input("Qual o jogo?"))
+
+    if(jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando Advinhação")
+        adivinhacao.jogar()
+if(__name__ == "__main__"):
+    escolher_jogo()
